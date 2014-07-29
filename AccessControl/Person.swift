9b4b5c9ca828d60b7246ec904abb2f744b467017
Ricky <ricky@dius.com.au>
2014-07-29 12:24:30 +1000

    access control demo

---
+++ AccessControl/Person.swift
@@ -0,0 +1,17 @@
+//
+//  Person.swift
+//  AccessControl
+//
+//  Created by Ricky Yim on 29/07/2014.
+//  Copyright (c) 2014 Ricky Yim. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    private let name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
---
+++ AccessControl/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  AccessControl
+//
+//  Created by Ricky Yim on 29/07/2014.
+//  Copyright (c) 2014 Ricky Yim. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+let person = Person(name: "John")
+println(person.name)
